9c8ef187533a86e392766bf620a418540a43aa7e
stubbiali <subbiali@phys.ethz.ch>
2024-05-09 00:34:50 +0200

    Add license.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
 This repository contains the data and scripts to produce the figures contained in the manuscript ``Exploring a high-level programming model for the NWP domain using ECMWF microphysics schemes'' by S. Ubbiali, C. Kühnlein, C. Schär, L. Schlemmer, T. C. Schulthess, M. Staneker, and H. Wernli, to be submitted for peer-review to Geoscientific Model Development (GMD). The article presents the Python rewrites using GT4Py of two IFS microphysics schemes from ECMWF: CLOUDSC and CLOUDSC2. For the latter, the corresponding tangent-linear and adjoint formulations are considered too.
 
 All figures can be generated in PDF format either by exporting diagrams from `src/img/cloudsc_paper.drawio`, or using the Python scripts available in `src/img/`. The Python scripts can be run as follows:
--- src/img/common.py
+++ src/img/common.py
@@ -1,4 +1,19 @@
 # -*- coding: utf-8 -*-
+#
+# Copyright 2022-2024 ETH Zurich
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from __future__ import annotations
 import os
 import pandas as pd
@@ -42,4 +57,6 @@ class Bar(BaseModel):
     @field_validator("y", mode="after")
     @classmethod
     def set_y(cls, v, info):
-        return get_value_from_dataset(info.data["ds_name"], info.data["col_name"], **info.data["constraints"])
+        return get_value_from_dataset(
+            info.data["ds_name"], info.data["col_name"], **info.data["constraints"]
+        )
